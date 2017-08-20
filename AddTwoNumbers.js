fdc910b4e7452dad843b7d828ccfb8a341e65fde
Sumitro Chatterjee <sumitrochatterjee@gmail.com>
2017-08-19 18:57:07 -0700

    update to fix empty case.

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -15,6 +15,7 @@ var addTwoNumbers = function(l1, l2) {
     var carry = 0;
     var sum = 0;
     while(l1 || l2 || carry) {
+        // console.log(l1, l2, lc, lr);
         sum = carry + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
         l1 = l1 ? l1.next : null; 
         l2 = l2 ? l2.next : null;
@@ -27,6 +28,7 @@ var addTwoNumbers = function(l1, l2) {
 
         lt = new ListNode(sum);
         if (lr) {
+            lr.next = lt;
             lr = lt;
         } else {
             lc = lt;
