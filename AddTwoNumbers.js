030125002503e6fa58334107c864df5cf6c7a492
Sumitro Chatterjee <sumitrochatterjee@gmail.com>
2017-08-19 18:45:25 -0700

    First draft solution to LeetCode

---
+++ AddTwoNumbers.js
@@ -0,0 +1,37 @@
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var lr = null;
+    var lc = null;
+    var lt = null;
+    var carry = 0;
+    var sum = 0;
+    while(l1 || l2 || carry) {
+        sum = carry + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+        l1 = l1 ? l1.next : null; 
+        l2 = l2 ? l2.next : null;
+        if (sum > 9) {
+            carry = 1;
+            sum = sum - 10;
+        } else {
+            carry = 0;
+        }
+
+        lt = new ListNode(sum);
+        if (lr) {
+            lr = lt;
+        } else {
+            lc = lt;
+            lr = lt;
+        }
+    }
+    return lc;
+};
